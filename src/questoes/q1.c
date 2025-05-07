0453588e6aa996cebe5c712f21a360ebfafab135
Law2509 <joao.rodrigues@icomp.ufam.edu.br>
2025-05-07 11:29:56 -0400

    Update q1.c

--- src/questoes/q1.c
+++ src/questoes/q1.c
@@ -1,3 +1,21 @@
 void questao1(unsigned int seed) {
+    No* raiz = NULL;
+    valores = srand(NULL);
+    int n = sizeof(valores) / sizeof(valores[0]);
 
+    for (int i = 0; i < n; i++) {
+        raiz = inserir(raiz, valores[i]);
+    }
+    printf("Pré-fixado: ");
+    pre_ordem(raiz);
+    printf("\n");
+
+    printf("Central: ");
+    em_ordem(raiz);
+    printf("\n");
+
+    printf("Pós-fixado: ");
+    pos_ordem(raiz);
+    printf("\n");
+  
 };
