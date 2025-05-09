d2225a654da8d57b7ac55439081f9c17e51cb42c
Law2509 <joao.rodrigues@icomp.ufam.edu.br>
2025-05-09 10:09:41 -0400

    Update q1.c

--- src/questoes/q1.c
+++ src/questoes/q1.c
@@ -1,38 +1,40 @@
-void questao1(unsigned int seed) {
+int main () { 
+    srand(time(NULL));
     No* raiz = NULL;
     int inseridos = 0;
-    int valores[20];
-
-    while (inseridos < 20){
-        int novo = rand() % 100;
+    int valores [20] = {0}; 
+    
+    while (inseridos < 20) {
+        int novo = rand () % 100;
         int repetido = 0;
-        for(int i = 0; i < inseridos; i++) {
-            if (valores [i] == novo) {
-                repetido = 1;
-                break;
+        for (int i= 0; i < inseridos; i++) {
+            if (valores[i] == novo) {
+                repetidos = 1;
+                break
             }
         }
-        if (!repetido) {
+        if(!repetido){
             valores[inseridos++] = novo;
-            raiz = inserir(raiz, novo);
+            raiz = inseridos(raiz, novo);
         }
     }
-
-    printf("valores inseridos:\n");
-    for (int i = 0; i < 20; i++) {
+    printf("Valores inseridos:\n");
+    for (int i = 0; i <20; i++) {
         printf("%d ", valores[i]);
     }
+    printf("\n\n");
+    
     printf("Pré-fixado: ");
     pre_ordem(raiz);
     printf("\n");
-
-    printf("Central: ");
+    
+    printf("Central: "); 
     em_ordem(raiz);
     printf("\n");
-
+    
     printf("Pós-fixado: ");
     pos_ordem(raiz);
     printf("\n");
-
+    
     return 0;
-};
+}    
