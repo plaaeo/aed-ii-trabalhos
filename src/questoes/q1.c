760777068c3aaef4f4fc6dfe486fc1442098ad92
Law2509 <joao.rodrigues@icomp.ufam.edu.br>
2025-05-11 14:11:26 -0400

    Update q1.c
    Vou me matar na frente de vocês

--- src/questoes/q1.c
+++ src/questoes/q1.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "abp.h"
-#include "abp.c"
-#include "vetor.c"
 #include "vetor.h"
+#include "abp.h"
 
 int main() {
-    srand(time(NULL));
-
     int tam = 20;
     vetor_t vet = vetor_criar(tam);
     vetor_gerar_aleatorio(vet, tam);
 
     printf("Vetor gerado:\n");
-    vetor_imprimir(vet, tam, 0, tam);
+    vetor_imprimir(vet, tam, 0, tam - 1);
 
     abp_t *abp = abp_criar();
-
     for (int i = 0; i < tam; i++) {
         abp_inserir(abp, vet[i]);
     }
 
-    printf("\nPré-fixado: ");
+    printf("\nPré-ordem:\n");
     abp_imprimir_pre_ordem(abp);
 
-    printf("Central: ");
+    printf("\nEm ordem:\n");
     abp_imprimir_em_ordem(abp);
 
-    printf("Pós-fixado: ");
+    printf("\nPós-ordem:\n");
     abp_imprimir_pos_ordem(abp);
 
     vetor_liberar(vet);
     abp_liberar(abp);
-
     return 0;
 }
