2a9878a0a6168c35c199d417422003882c02795b
Becky0757 <rebeccaimee.lima@gmail.com>
2025-05-13 10:35:10 -0400

    Update pct.h

--- src/pct.h
+++ src/pct.h
@@ -1,1 +1,22 @@
-abp_pct_t* pct_criar_no(Pacote pacote)
+#pragma once
+
+typedef struct Pacotes Pacote;
+typedef struct abp_pct_t abp_pct_t;
+
+//cria um no
+abp_pct_t* pct_criar_no(Pacote pacote);
+
+// insere um valor na árvore
+abp_pct_t* pct_abp_inserir(abp_pct_t* raiz, Pacote pacote);
+
+// busca um valor na árvore
+abp_pct_t* pct_abp_buscar(abp_pct_t* raiz, int ID);
+
+// remove um valor da arvore
+abp_pct_t* pct_abp_remover(abp_pct_t* raiz, Pacote pacote);
+
+//ordena parcialmente os pacotes pelo id
+Pacote pct_ordenar_parcial_id(int tam, Pacote pacotes[tam]);
+
+//ordena todos os pacotes por id
+Pacote pct_ordenar_id(int tam, Pacote pacotes[tam]);
