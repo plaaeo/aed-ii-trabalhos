e9e0d69b6a34b9eee128ca062f2c2a78197bf3d8
Becky0757 <rebeccaimee.lima@gmail.com>
2025-07-16 16:14:06 -0400

    Update ciclo.c
    reutilizando o grafo.h e grafo.c dessa vez

--- src/ciclo.c
+++ src/ciclo.c
@@ -1,41 +1,41 @@
 #include "ciclo.h"
+
+#include <stddef.h>
 #include <stdlib.h>
 
-#define max_vertices 100
-
-static bool visitado[max_vertices];
-static bool encontrouCiclo = false;
-
-static void inicializarVisitados(int n) {
-    for (int i = 0; i < n; i++) {
-        visitado[i] = false;
-    }
-}
-
-static void dfsDetectaCiclo(int atual, int pai, int **grafo, int n) {
+/// Função auxiliar de DFS para detecção de ciclos
+static bool dfs_ciclo(const grafo_t *grafo, size_t atual, size_t pai, bool *visitado) {
     visitado[atual] = true;
 
-    for (int i = 0; i < n; i++) {
-        if (grafo[atual][i]) {
-            if (!visitado[i]) {
-                dfsDetectaCiclo(i, atual, grafo, n);
-            } else if (i != pai) {
-                encontrouCiclo = true;
+    for (size_t vizinho = 0; vizinho < grafo_tamanho(grafo); vizinho++) {
+        if (grafo_tem_aresta(grafo, atual, vizinho)) {
+            if (!visitado[vizinho]) {
+                if (dfs_ciclo(grafo, vizinho, atual, visitado)) {
+                    return true;
+                }
+            } else if (vizinho != pai) {
+                return true; // Encontrou um ciclo (tamanho ≥ 3)
             }
         }
     }
+
+    return false;
 }
 
-bool possuiCiclo(int **grafo, int n) {
-    inicializarVisitados(n);
-    encontrouCiclo = false;
+bool grafo_tem_ciclo(const grafo_t *grafo) {
+    size_t n = grafo_tamanho(grafo);
+    bool *visitado = calloc(n, sizeof(bool));
+    if (!visitado) return false;
 
-    for (int i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         if (!visitado[i]) {
-            dfsDetectaCiclo(i, -1, grafo, n);
-            if (encontrouCiclo) return true;
+            if (dfs_ciclo(grafo, i, (size_t)-1, visitado)) {
+                free(visitado);
+                return true;
+            }
         }
     }
 
+    free(visitado);
     return false;
 }
